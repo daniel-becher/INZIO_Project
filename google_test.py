d3c96cb1e91c97aae0b2e8bd5caea24dab476814
Daniel Becher <daniel_becher_dev@gmail.com>
2026-05-01 16:18:50 +0200

    MBY

--- google_test.py
+++ google_test.py
@@ -21,11 +21,17 @@ def google_scrape(query: str):
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
     
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
     driver = webdriver.Chrome(options=options)
     results = []
 
     try:
+        driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+    "source": "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})"
+})
         url = f"https://www.google.com/search?q={query}&hl=cs"
         driver.get(url)
 

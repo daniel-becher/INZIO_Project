2aab7eec4a5c4397b195fc00cbe1bcc9a24199d4
Daniel Becher <daniel_becher_dev@gmail.com>
2026-05-01 15:53:05 +0200

    Fix: Force rename Templates to lowercase templates

--- google_test.py
+++ google_test.py
@@ -17,7 +17,6 @@ template = Jinja2Templates (directory="templates")
 def google_scrape(query: str):
     options = Options()
 
-    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")

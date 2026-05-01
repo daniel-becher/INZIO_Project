73118b6add8b16cba4fb25e17d2e013eff683b5c
Daniel Becher <daniel_becher_dev@gmail.com>
2026-05-01 16:17:06 +0200

    SHOWTIME

--- google_test.py
+++ google_test.py
@@ -17,23 +17,15 @@ template = Jinja2Templates (directory="templates")
 def google_scrape(query: str):
     options = Options()
 
-    options.add_argument("--headless=new") # Nový, lépe maskovaný headless mód
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
     
-    # Maskování robota
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option('useAutomationExtension', False)
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
     driver = webdriver.Chrome(options=options)
     results = []
 
     try:
-        driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
-    "source": "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})"
-})
         url = f"https://www.google.com/search?q={query}&hl=cs"
         driver.get(url)
 

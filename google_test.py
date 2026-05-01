66557da00dbcd012bc0fbdfa4aaa4e990cf51dc6
Daniel Becher <daniel_becher_dev@gmail.com>
2026-05-01 15:38:52 +0200

    Fix: Headless mode for Docker

--- google_test.py
+++ google_test.py
@@ -17,8 +17,10 @@ template = Jinja2Templates (directory="templates")
 def google_scrape(query: str):
     options = Options()
 
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
